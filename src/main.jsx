460735869b276d178dfba4505fb1a24c82a5543c
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2024-01-16 19:37:14 +0600

    web page width adjusted

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,9 @@ import MyRouts from './MyRouts/MyRouts.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <div className='max-w-7xl mx-auto'>
+
      <RouterProvider router={MyRouts} />
+    </div>
   </React.StrictMode>,
 )
