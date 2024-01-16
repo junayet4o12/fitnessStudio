b71b1fee962386fae6ef69eb66f4bb29a059b4ca
Md-Arman01 <137868415+Md-Arman01@users.noreply.github.com>
2024-01-16 20:53:38 +0600

    added login and register page

---
+++ src/Components/GoogleSignIn/GoogleSignIn.jsx
@@ -0,0 +1,13 @@
+import { FcGoogle } from "react-icons/fc";
+
+const GoogleSignIn = () => {
+  return (
+    <div>
+      <button className="btn btn-outline btn-info normal-case w-full">
+        <FcGoogle className="text-xl"></FcGoogle>Login With Google
+      </button>
+    </div>
+  );
+};
+
+export default GoogleSignIn;
--- src/MyRouts/MyRouts.jsx
+++ src/MyRouts/MyRouts.jsx
@@ -2,34 +2,36 @@
 
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../MainLayout/MainLayout";
+import LogIn from "../Pages/AuthPages/LogIn/LogIn";
+import Register from "../Pages/AuthPages/Register/Register";
 
 const MyRouts = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout></MainLayout>,
-      children: [
-        {
-            path: '/',
-            element: <div>This is home</div>
-        },
-        {
-            path: '/services',
-            element: <div>This is Services</div>
-        },
-        {
-            path: '/contactus',
-            element: <div>This is Contact Us</div>
-        },
-        {
-            path: '/register',
-            element: <div>This is Register</div>
-        },
-        {
-            path: '/login',
-            element: <div>This is Login</div>
-        },
-      ]
-    },
-  ]);
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <div>This is home</div>,
+      },
+      {
+        path: "/services",
+        element: <div>This is Services</div>,
+      },
+      {
+        path: "/contact_us",
+        element: <div>This is Contact Us</div>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/login",
+        element: <LogIn></LogIn>,
+      },
+    ],
+  },
+]);
 
 export default MyRouts;
