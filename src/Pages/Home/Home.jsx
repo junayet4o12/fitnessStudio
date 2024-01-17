a78e2275299ead6af7097d418f3547bbe267aa10
junayet <muhammadjunayetmaruf@gmail.com>
2024-01-17 17:12:02 +0600

    how it works section has completed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 // import React from 'react';
 
 import Banner from "../../Components/Banner/Banner";
+import HowItWorks from "../../Components/HowItWorks/HowItWorks";
 import Star from "../../Components/StarFeatures/Star";
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Star></Star>
+            <HowItWorks></HowItWorks>
         </div>
     );
 };
