81cd74229d5e34f1caf9d0bcb16b604690ff4dfd
JubayerRahman <jubayerr398@gmail.com>
2024-01-17 15:17:39 +0600

    Made the about us page with complete responsiveness

--- src/MyRouts/MyRouts.jsx
+++ src/MyRouts/MyRouts.jsx
@@ -5,6 +5,7 @@ import MainLayout from "../MainLayout/MainLayout";
 import LogIn from "../Pages/AuthPages/LogIn/LogIn";
 import Register from "../Pages/AuthPages/Register/Register";
 import Home from "../Pages/Home/Home";
+import Aboutus from "../Pages/About us/Aboutus";
 
 const MyRouts = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ const MyRouts = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
+      },
+      {
+        path: "/aboutUs",
+        element: <Aboutus/>
       },
       {
         path: "/services",
