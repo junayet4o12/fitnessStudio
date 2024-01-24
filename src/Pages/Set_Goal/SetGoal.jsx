a2b567e29c976009019df205b18952568381388a
Md-Arman01 <137868415+Md-Arman01@users.noreply.github.com>
2024-01-24 19:45:12 +0600

    [feat](set_goal): added activity section

--- src/Pages/Set_Goal/SetGoal.jsx
+++ src/Pages/Set_Goal/SetGoal.jsx
@@ -1,11 +1,13 @@
+import Activity from "./Activity";
 import ExercisePlan from "./ExercisePlan";
 import WeightGain from "./WeightGain";
 
 const SetGoal = () => {
   return (
-    <div className="my-10">
+    <div className="mt-10 mb-20">
       <ExercisePlan></ExercisePlan>
       <WeightGain></WeightGain>
+      <Activity></Activity>
     </div>
   );
 };
--- src/Pages/Set_Goal/WeightGain.jsx
+++ src/Pages/Set_Goal/WeightGain.jsx
@@ -4,11 +4,7 @@ import Title from "../../Components/Title/Title";
 const WeightGain = () => {
   const buttonStyle =
     "p-2 xs:p-2.5 transition-all duration-500 w-[110px] xs:w-[130px] font-bold text-white rounded border-[3px] active:bg-[#ff470470] active:scale-90";
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  const { register, handleSubmit } = useForm();
 
   const onSubmit = (data) => {
     console.log(data);
@@ -16,7 +12,7 @@ const WeightGain = () => {
 
   return (
     <div className="px-4 lg:px-10  lg:mt-10">
-      <Title title="Weight Gain"></Title>
+      <Title title="Manage Weight"></Title>
       <div className="flex items-center justify-between gap-10 mt-5">
         <div className="flex-1">
           <img
@@ -31,30 +27,26 @@ const WeightGain = () => {
             <span className="text-primary">Building</span> a Stronger You
           </h1>
           <p className="text-base text-gray-600 font-medium mt-5">{`Are you tired of struggling to put on weight? Whether you're looking to gain muscle mass, overcome a medical condition, or simply achieve a more robust physique, our guide has got you covered! "Unlocking Healthy Weight Gain" is your go-to resource for understanding the principles of effective weight gain without compromising your health.`}</p>
-          <h1 className="text-base text-black font-medium mt-5 mb-2">
-            How many kg do you want to gain in 1 month?{" "}
+          <h1 className="text-base text-black font-semibold mt-5 mb-2">
+            What are you willing to do?{" "}
             <span className="text-primary text-2xl">*</span>
           </h1>
           <form onSubmit={handleSubmit(onSubmit)}>
             <div className="flex items-center gap-2">
               <div>
-                <input
-                  type="number"
-                  {...register("gain_kg", { required: true })}
-                  placeholder="00 kg"
-                  className="input input-bordered w-full"
-                />
+                <select
+                  {...register("gain")}
+                  className="border rounded-md w-60 h-[51px] px-2 focus:border-primary">
+                  <option value="Gain Weight">Gain Weight</option>
+                  <option value="Lose Weight">Lose Weight</option>
+                  <option value="Maintain Weight">Maintain Weight</option>
+                </select>
               </div>
               <button
                 className={`${buttonStyle} bg-primary hover:bg-[#ff470436]  border-transparent hover:border-primary hover:text-black `}>
                 Gain
               </button>
             </div>
-            {errors.gain_kg && (
-              <span className="text-red-400 text-xs">
-                This field is required
-              </span>
-            )}
           </form>
         </div>
       </div>
