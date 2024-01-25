3f2c00c682fd37020e691d4575038225565e83e1
Barkat ullah <barkatullah585464@gmail.com>
2024-01-25 19:23:17 +0600

    foolish with responsive layout

--- src/Pages/TrackProgress/TrackProgress.jsx
+++ src/Pages/TrackProgress/TrackProgress.jsx
@@ -18,77 +18,77 @@ const TrackProgress = () => {
     text: {
       fill: "#FF4804",
       fontSize: "20px",
-      fontFamily: "Poppins",
     },
   };
 
   return (
-    <div className="container mx-auto px-2 flex flex-col lg:flex-row">
-      <div className="lg:w-1/2">
-        <div className="flex flex-col lg:flex-row justify-around gap-2 ">
-          <div className="lg:w-1/2 ">
-            <div className="container mx-auto px-2 flex my-[100px]">
-              <div className="w-1/2  border border-blue-500">
-                <div className="flex flex-row justify-around gap-2">
-                  <div className="w-1/2 ">
-                    <div className={`${cardStyle}`}>
-                      <div className="mb-2 flex justify-center items-center space-x-1 ">
-                        <IoFootstepsOutline className="text-primary text-2xl " />
-                        <h3 className="text-lg font-medium text-gray-700">
-                          Steps
-                        </h3>
-                      </div>
-                      <div>
-                        <CircularProgressbar
-                          styles={progressBarStyles}
-                          value={totalPercentage}
-                          text={`${totalPercentage.toFixed(1)}%`}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className=" lg:w-1/2 flex flex-col space-y-6">
-                    <div className="mt-6">
-                      <div className="card bg-orange-50 mb-2">
-                        <div className="card-body flex flex-row justify-center items-center">
-                          <div className="card-actions justify-start">
-                            <FaCarSide className="text-primary text-2xl" />
-                          </div>
-                          <div>
-                            <p className="text-xl font-semibold">Distance</p>
-                            <span className="text-xl font-semibold bmiNumber">
-                              6.4 kilometers
-                            </span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
+    <div>
+      <div className="flex flex-col justify-center items-center mt-4">
+        <h2 className="font-bold text-3xl md:text-left md:text-5xl">
+          Daily <span className="text-primary">Activity</span>
+        </h2>
+        <p className="font-medium text-xl mt-2">
+          Here are your Daily Activities. Check out the exciting things you've
+          <br />
+          accomplished today and plan for more success tomorrow.
+        </p>
+      </div>
 
-                    <div className="">
-                      <div className="card bg-sky-50 mb-1">
-                        <div className="card-body flex flex-row justify-center items-center">
-                          <div className="card-actions justify-start">
-                            <GiNightSleep className="text-primary text-2xl" />
-                          </div>
-                          <div>
-                            <p className="text-xl font-semibold">Sleep</p>
-                            <span className="text-xl font-semibold bmiNumber">
-                              8.4 Hours
-                            </span>
-                          </div>
-                        </div>
-                      </div>
+      <div className="container mx-auto px-2 md:py-4 flex flex-col lg:flex-row">
+        <div className="lg:w-1/2">
+          <div className="flex flex-col lg:flex-row justify-around gap-2 ">
+            <div className="lg:w-1/2 ">
+              <div className={`${cardStyle}`}>
+                <div className="mb-2 flex justify-center items-center space-x-1 ">
+                  <IoFootstepsOutline className="text-primary text-2xl " />
+                  <h3 className="text-lg font-medium text-gray-700">Steps</h3>
+                </div>
+                <div>
+                  <CircularProgressbar
+                    styles={progressBarStyles}
+                    value={totalPercentage}
+                    text={`${totalPercentage.toFixed(1)}%`}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className=" lg:w-1/2 flex flex-col space-y-6">
+              <div className="mt-6">
+                <div className="card bg-orange-50 mb-2">
+                  <div className="card-body flex flex-row justify-center items-center">
+                    <div className="card-actions justify-start">
+                      <FaCarSide className="text-primary text-2xl" />
+                    </div>
+                    <div>
+                      <p className="text-xl font-semibold">Distance</p>
+                      <span className="text-xl font-semibold">
+                        6.4 kilometers
+                      </span>
                     </div>
                   </div>
                 </div>
-                <HeartRate />
               </div>
 
-              <div className="lg:w-1/2 ">
-                <ChartProgress />
+              <div className="">
+                <div className="card bg-sky-50 mb-1">
+                  <div className="card-body flex flex-row justify-center items-center">
+                    <div className="card-actions justify-start">
+                      <GiNightSleep className="text-primary text-2xl" />
+                    </div>
+                    <div className="">
+                      <p className="text-xl font-semibold">Sleep</p>
+                      <span className="text-xl font-semibold">8.4 Hours</span>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+          <HeartRate />
+        </div>
+
+        <div className="lg:w-1/2 ">
+          <ChartProgress />
         </div>
       </div>
     </div>
