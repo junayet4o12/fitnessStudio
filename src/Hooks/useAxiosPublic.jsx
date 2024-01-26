37a96af95702005ee3dc2b99d209600cb3d928c4
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2024-01-26 18:25:31 +0600

    withcredentials deleted

--- src/Hooks/useAxiosPublic.jsx
+++ src/Hooks/useAxiosPublic.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
+
+// import React from 'react';
 const axiosPublic = axios.create({
     baseURL: 'https://fitnessstudio-bacend.vercel.app',
-    withCredentials:true,
 })
 const useAxiosPublic = () => {
     return axiosPublic
