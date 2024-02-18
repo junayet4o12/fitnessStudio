7c0176205d3acb09541d57f39e36ff5082825486
JubayerRahman <jubayerr398@gmail.com>
2024-02-18 08:44:47 +0600

    added the besic structure of the product listing feature

--- src/Pages/ProductsCollections/ProductsCollections.jsx
+++ src/Pages/ProductsCollections/ProductsCollections.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ProductsCollections = () => {
   return (
-    <div className='p-[10px]'>
+    <div className='p-[10px] '>
       <h1>Products Collections.</h1>
     </div>
   )
