81c142231fbaa96373aae165278d1269cd809f3c
junayet <muhammadjunayetmaruf@gmail.com>
2024-02-21 10:33:54 +0600

    [Update]:[UserProfile.jsx]:UserProfile section has updated

--- src/Components/Message/MessageBox.jsx
+++ src/Components/Message/MessageBox.jsx
@@ -11,7 +11,6 @@ const MessageBox = ({ userData, friendData, messages = [], refetch, scrollToTop 
     const navigate = useNavigate()
     const [message, setMessage] = useState('')
     const chatContainerRef = useRef(null);
-    const socket = io('wss://green-aquamarine-pint.glitch.me')
     useEffect(() => {
         socket.on('refetch', (message) => {
             refetch()
