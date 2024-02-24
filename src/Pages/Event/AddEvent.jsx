8dab2ec7faa01cae048a368624656946809be6d4
Md-Arman01 <137868415+Md-Arman01@users.noreply.github.com>
2024-02-24 17:49:21 +0600

    [feat](event): added event form in admin deshboard

--- src/Pages/Event/AddEvent.jsx
+++ src/Pages/Event/AddEvent.jsx
@@ -1,11 +1,182 @@
-
+import { useState } from "react";
+import useAuth from "../../Hooks/useAuth";
+import Title from "../../Components/Title/Title";
+import { useForm } from "react-hook-form";
+import DatePicker from "react-datepicker";
+import { FaCloudUploadAlt } from "react-icons/fa";
+import "react-datepicker/dist/react-datepicker.css";
+import axios from "axios";
 
 const AddEvent = () => {
-    return (
-        <div>
-            <h1>this is add event page</h1>
+  const { user } = useAuth();
+  const [startDate, setStartDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const imgHostingKey = import.meta.env.VITE_IMG_HOSTING_KEY;
+  const imgHostingApi = `https://api.imgbb.com/1/upload?key=${imgHostingKey}`;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const image = { image: data?.image[0] };
+    console.log({ image: data?.image[0]?.name });
+    const res = await axios.post(imgHostingApi, image, {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    });
+    const imageURL = res?.data?.data?.display_url;
+    console.log(data, imageURL);
+  };
+
+  const buttonStyle =
+    "p-2 xs:p-2.5 transition-all duration-500 w-full font-bold text-white rounded border-[3px] active:bg-[#ff470470] active:scale-90";
+
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-full px-20">
+          {/* <Helmet>
+            <title>Product form - FitnessStudion</title>
+        </Helmet> */}
+          <Title title="Add Event"></Title>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-2 ">
+            <div className="flex flex-col md:flex-row gap-2 w-full">
+              <div className="w-full">
+                <label className="text-xl font-[600]" htmlFor="name">
+                  Event Name <span className="text-red-600 text-xl">*</span>{" "}
+                </label>
+                <input
+                  {...register("event_name", { required: true })}
+                  className="text-xl p-[10px] w-full border-2 border-primary rounded-md bg-transparent my-2"
+                  type="text"
+                  placeholder="Event name"
+                />
+                {errors.event_name?.type === "required" && (
+                  <span className="text-red-600 text-xs">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              <div className="w-full">
+                <label className="text-xl font-[600]" htmlFor="price">
+                  Price <span className="text-red-600 text-xl">*</span>
+                </label>
+                <input
+                  {...register("event_price", { required: true })}
+                  className="text-xl p-[10px] w-full border-2 border-primary rounded-md bmiNumber bg-transparent  my-2"
+                  type="number"
+                  placeholder="৳ 00"
+                />
+                {errors.event_price?.type === "required" && (
+                  <span className="text-red-600 text-xs">
+                    This field is required
+                  </span>
+                )}
+              </div>
+              <div className="w-full">
+                <label className="text-xl font-[600]" htmlFor="Tickets">
+                  Tickets <span className="text-red-600 text-xl">*</span>
+                </label>
+                <input
+                  {...register("tickets", { required: true })}
+                  className="text-xl p-[10px] w-full border-2 border-primary rounded-md bmiNumber bg-transparent  my-2"
+                  type="number"
+                  placeholder="Tickets"
+                />
+                {errors.tickets?.type === "required" && (
+                  <span className="text-red-600 text-xs">
+                    This field is required
+                  </span>
+                )}
+              </div>
+            </div>
+            <label className="text-xl font-[600]" htmlFor="description">
+              Description <span className="text-red-600 text-xl">*</span>
+            </label>
+            <textarea
+              {...register("description", { required: true })}
+              className="text-xl p-[10px] w-full border-2 border-primary rounded-md h-[250px] max-h-[250px] min-h-[250px] text-left bg-transparent  my-2"
+              placeholder="Description"
+              id="description"
+              name="description"
+            />
+            {errors.description?.type === "required" && (
+              <span className="text-red-600 text-xs">
+                This field is required
+              </span>
+            )}
+            <div className="flex flex-col md:flex-row justify-between gap-2 w-full mt-2">
+              <div className="flex-1">
+                <label className="text-xl font-[600]" htmlFor="image">
+                  Select Event Photo{" "}
+                  <span className="text-red-600 text-xl">*</span>
+                </label>
+                <div className="relative group border-2 border-dashed border-primary rounded-lg py-2 my-2">
+                  <div className="flex items-center justify-center gap-5 absolute left-[162px]  group-hover:cursor-pointer">
+                    <FaCloudUploadAlt className="text-2xl"></FaCloudUploadAlt>
+                    <h1 className="text-lg font-medium">Upload Photo</h1>
+                  </div>
+                  <input
+                    type="file"
+                    {...register("image", { required: true })}
+                    className="w-full opacity-0  group-hover:cursor-pointer"
+                  />
+                </div>
+                {errors.image?.type === "required" && (
+                  <span className="text-red-600 text-xs ">Select an image</span>
+                )}
+              </div>
+              <div className=" flex-1">
+                <div>
+                  <label className="text-xl font-[600]" htmlFor="event_start">
+                    Event Start <span className="text-red-600 text-xl">*</span>{" "}
+                  </label>
+                </div>
+                <DatePicker
+                  required
+                  selected={startDate}
+                  onChange={(date) => setStartDate(date)}
+                  endDate={endDate}
+                  minDate={new Date()}
+                  showDisabledMonthNavigation
+                  placeholderText="Select Date"
+                  className="text-black hover:cursor-pointer pl-[198px] py-[13px] rounded-md my-2 bg-gray-100 w-[485px]"
+                />
+              </div>
+              <div className="flex-1">
+                <div>
+                  <label className="text-xl font-[600]" htmlFor="event_end">
+                    Event End <span className="text-red-600 text-xl">*</span>
+                  </label>
+                </div>
+                <DatePicker
+                  required
+                  selected={endDate}
+                  onChange={(date) => setEndDate(date)}
+                  selectsEnd
+                  startDate={startDate}
+                  endDate={endDate}
+                  minDate={new Date(startDate)}
+                  placeholderText="Select Date"
+                  className="text-black hover:cursor-pointer pl-[198px] py-[13px] rounded-md my-2 bg-gray-100 w-[485px]"
+                />
+              </div>
+            </div>
+            <button
+              type="submit"
+              className={`${buttonStyle} bg-primary/40 hover:bg-primary  border-primary hover:border-transparent mt-5`}>
+              Add Event
+            </button>
+          </form>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default AddEvent;
