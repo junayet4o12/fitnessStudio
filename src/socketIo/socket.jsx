9d2f2e624a0e9abe0d0d3ccc7736a0e184c4e774
Jubayer-Ahmed-Sajid <jubayerahmed661ac@gmail.com>
2024-03-02 10:53:22 +0600

    changed are saved

--- src/socketIo/socket.jsx
+++ src/socketIo/socket.jsx
@@ -1,10 +1,6 @@
 import { io } from "socket.io-client";
 
-// export const socket = io('http://localhost:3000')
 export const socket = io('wss://green-aquamarine-pint.glitch.me')
 
-// const socket = io('socket Link');
-// socket.on('message', (value)=> {
-//     console.log(value);
-// })
+
 
